6dfff96dcd38e59273772e765daca66fe6184581
Mantagar <mantagar@proton.me>
2026-07-24 18:42:24 +0200

    Setup project

---
+++ src/main/java/org/mantagar/squadselector/SquadSelectorApplication.java
@@ -0,0 +1,13 @@
+package org.mantagar.squadselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadSelectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquadSelectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mantagar/squadselector/SquadSelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mantagar.squadselector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadSelectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
